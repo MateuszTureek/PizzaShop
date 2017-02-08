d2b307e88b242b0fb0572fefe225a28793c52cf0
MT <matiaz17@gmail.com>
2017-02-08 21:01:31 +0100

    Begin project

--- PizzaShop/Controllers/HomeController.cs
+++ PizzaShop/Controllers/HomeController.cs
@@ -10,19 +10,7 @@ namespace PizzaShop.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            string name = "ala";
 
             return View();
         }
